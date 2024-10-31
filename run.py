cebfcd9c4003b8b820ddd494048d5af5efe2f49e
Liam Harper-McCabe <liamharpermccabe@Liams-MacBook-Air.local>
2024-10-31 18:46:48 -0400

    Final formatting for draft

--- run.py
+++ run.py
@@ -108,17 +108,6 @@ class BasicPropositions:
     
 
 #Define Variables
-Q = BasicPropositions("Q")
-T = BasicPropositions("T")   
-W = BasicPropositions("W")
-E = BasicPropositions("E")
-P = BasicPropositions("P")
-C = BasicPropositions("C")
-S = BasicPropositions("S")
-t = BasicPropositions("t")
-L = BasicPropositions("L")
-
-
 C1_vprime = BasicPropositions("C1_vprime")  
 C2_vprime = BasicPropositions("C2_vprime")  
 
@@ -244,7 +233,25 @@ class Lead:
             self.L = True
 
 
+# Different classes for propositions are useful because this allows for more dynamic constraint creation
+# for propositions within that class. For example, you can enforce that "at least one" of the propositions
+# that are instances of this class must be true by using a @constraint decorator.
+# other options include: at most one, exactly one, at most k, and implies all.
+# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
 
+@proposition(E)
+class FancyPropositions:
+
+    def __init__(self, data):
+        self.data = data
+
+    def _prop_name(self):
+        return f"A.{self.data}"
+    
+
+
+@constraint.at_least_one(E)
+@proposition(E)
 def constraints():
 
     constraint_expression1 = C1_vprime | (~C1_vprime & C2_vprime)
@@ -279,59 +286,39 @@ def constraints():
 
     return E
 
-
-# Different classes for propositions are useful because this allows for more dynamic constraint creation
-# for propositions within that class. For example, you can enforce that "at least one" of the propositions
-# that are instances of this class must be true by using a @constraint decorator.
-# other options include: at most one, exactly one, at most k, and implies all.
-# For a complete module reference, see https://bauhaus.readthedocs.io/en/latest/bauhaus.html
-@constraint.at_least_one(E)
-@proposition(E)
-class FancyPropositions:
-
-    def __init__(self, data):
-        self.data = data
-
-    def _prop_name(self):
-        return f"A.{self.data}"
-
-# Call your variables whatever you want
-
-
-
 # Build an example full theory for your setting and return it.
 #
 #  There should be at least 10 variables, and a sufficiently large formula to describe it (>50 operators).
 #  This restriction is fairly minimal, and if there is any concern, reach out to the teaching staff to clarify
 #  what the expectations are.
 def example_theory():
-    # Add custom constraints by creating formulas with the variables you created. 
-    E.add_constraint((a | b) & ~x)
-    # Implication
-    E.add_constraint(y >> z)
-    # Negate a formula
-    E.add_constraint(~(x & y))
-    # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
-    # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
-    constraint.add_exactly_one(E, a, b, c)
+    # # Add custom constraints by creating formulas with the variables you created. 
+    # E.add_constraint((a | b) & ~x)
+    # # Implication
+    # E.add_constraint(y >> z)
+    # # Negate a formula
+    # E.add_constraint(~(x & y))
+    # # You can also add more customized "fancy" constraints. Use case: you don't want to enforce "exactly one"
+    # # for every instance of BasicPropositions, but you want to enforce it for a, b, and c.:
+    # constraint.add_exactly_one(E, a, b, c)
 
-    return E
+    # return E
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    T = example_theory()
-    # Don't compile until you're finished adding all your constraints!
-    T = T.compile()
-    # After compilation (and only after), you can check some of the properties
-    # of your model:
-    print("\nSatisfiable: %s" % T.satisfiable())
-    print("# Solutions: %d" % count_solutions(T))
-    print("   Solution: %s" % T.solve())
+#     T = example_theory()
+#     # Don't compile until you're finished adding all your constraints!
+#     T = T.compile()
+#     # After compilation (and only after), you can check some of the properties
+#     # of your model:
+#     print("\nSatisfiable: %s" % T.satisfiable())
+#     print("# Solutions: %d" % count_solutions(T))
+#     print("   Solution: %s" % T.solve())
 
-    print("\nVariable likelihoods:")
-    for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
-        # Ensure that you only send these functions NNF formulas
-        # Literals are compiled to NNF here
-        print(" %s: %.2f" % (vn, likelihood(T, v)))
-    print()
+#     print("\nVariable likelihoods:")
+#     for v,vn in zip([a,b,c,x,y,z], 'abcxyz'):
+#         # Ensure that you only send these functions NNF formulas
+#         # Literals are compiled to NNF here
+#         print(" %s: %.2f" % (vn, likelihood(T, v)))
+#     print()
